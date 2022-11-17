5e585c6599e8c3c3113b0e4805296ad3c3468380
Emmanuel C <misterium273@gmail.com>
2022-11-17 13:23:22 -0400

    se eliminaron 2 letras

--- src/components/DeleteWindow.jsx
+++ src/components/DeleteWindow.jsx
@@ -5,7 +5,7 @@ const DeleteWindow = ({ setActivateDelete, eliminate }) => {
     <div className="content--form">
       <div className="content--data-form">
         <h2>Seguro que lo deseas eliminar</h2>
-        <small>no te hagas a los capos eh xd</small>
+        <small>no te hagas a los capos eh...</small>
         <div className="content--button">
           <button onClick={() => eliminate()}>eliminar</button>
           <button onClick={() => setActivateDelete(null)}>cancelar</button>
